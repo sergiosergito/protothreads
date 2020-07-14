207f147c5604fb25c807d279913af6971d94ad19
serginho6x <serginho_6x@hotmail.com>
2020-07-14 15:30:27 -0400

    blue led

--- ProtoThreadsDemo.ino
+++ ProtoThreadsDemo.ino
@@ -1,26 +1,32 @@
 #include <pt.h>
+
 #define ONE_SECOND 1000
 #define HALF_SECOND 500
 #define QUARTER_OF_A_SECOND 250
+#define TWO_SEVENTYEIGHT_SECONDS 2780
 
 int redLED = 10;
 int yellowLED = 9;
 int greenLED = 8;
+int blueLED = 6;
 
 struct pt redThread;
 struct pt yellowThread;
 struct pt greenThread;
+struct pt blueThread;
 
 void setup() {
   PT_INIT(&redThread);
   PT_INIT(&yellowThread);
   PT_INIT(&greenThread);
+  PT_INIT(&blueThread);
 }
 
 void loop() {
   parpadeo(&redThread);
   parpadeo2(&yellowThread);
   parpadeo3(&greenThread);
+  parpadeo4(&blueThread);
 }
 
 void parpadeo(struct pt *protothread){
@@ -67,6 +73,22 @@ void parpadeo3(struct pt *protothread){
       digitalWrite(greenLED, LOW);
       timeAtMillisTaken = millis();
       PT_WAIT_UNTIL(protothread, (millis() - timeAtMillisTaken) >= QUARTER_OF_A_SECOND);
+  }while(true);
+  PT_END(protothread);
+}
+
+void parpadeo4(struct pt *protothread){
+  PT_BEGIN(protothread);
+  static long timeAtMillisTaken = 0;
+  pinMode(blueLED, OUTPUT);
+  do{
+      digitalWrite(blueLED, HIGH);
+      timeAtMillisTaken = millis();
+      PT_WAIT_WHILE(protothread, (millis() - timeAtMillisTaken)< TWO_SEVENTYEIGHT_SECONDS);
+      
+      digitalWrite(blueLED, LOW);
+      timeAtMillisTaken = millis();
+      PT_WAIT_UNTIL(protothread, (millis() - timeAtMillisTaken) >= TWO_SEVENTYEIGHT_SECONDS);
   }while(true);
   PT_END(protothread);
 }
